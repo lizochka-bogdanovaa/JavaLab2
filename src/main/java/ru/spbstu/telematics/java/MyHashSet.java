2b2161924f54f2cd82dad4657f22d3a00b158dbf
lizochka-bogdanovaa <lizabog2004@gmail.com>
2024-11-17 23:22:32 +0300

    Initial commit

---
+++ src/main/java/ru/spbstu/telematics/java/MyHashSet.java
@@ -0,0 +1,242 @@
+package ru.spbstu.telematics.java;
+
+import java.util.*;
+
+public class MyHashSet<T> implements Set<T>, Iterable<T>
+{
+
+    private int size; //текущее количество элементов в множестве
+    private static final int INITIAL_CAPACITY = 16; //количестао мест в массиве всего
+    private static final float LOAD_FACTOR = 0.75f; //когда массив будет заполнен на 75%, произойдет увеличение емкости (resize)
+    private Object[] table;
+
+    public MyHashSet(){ //OK
+        table = new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    private int hash(Object key) { //OK
+        return (key == null) ? 0 : Math.abs(key.hashCode()) % table.length;
+    }
+
+    private int find(Object key) { //OK
+        int index = hash(key);
+        Object[] sameHash = (Object[]) table[index]; //элементы с одинаковым хеш-кодом
+        if (sameHash != null) {
+            for (int i = 0; i < sameHash.length; i++) {
+                if (sameHash[i] != null && sameHash[i].equals(key)) {
+                    return index;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void resize() { //OK
+        Object[] oldTable = table;
+        table = new Object[table.length * 2];
+        size = 0;
+        for (Object sameHash : oldTable) {
+            if (sameHash != null) {
+                for (Object key : (Object[]) sameHash) {
+                    add((T) key);
+                }
+            }
+        }
+    }
+
+    @Override
+    public int size() { //OK
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() { //OK
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object key) {//OK
+        return find(key) != -1;
+    }
+
+    @Override
+    public Iterator<T> iterator() {//OK
+        return new MyHashSetIterator();
+    }
+
+    private class MyHashSetIterator implements Iterator<T>{//OK
+
+        private int currentSameHashIndex = 0;
+        private int currentElementIndex = -1;
+        private boolean canRemove = false;
+        private T nextElement;
+
+        @Override
+        public boolean hasNext() {
+            findElement();
+            return nextElement != null;
+        }
+
+        @Override
+        public T next() {
+            findElement();
+            if(nextElement==null){
+                throw new NoSuchElementException();
+            }
+            T result = nextElement;
+            nextElement = null;
+            return result;
+        }
+
+        @Override
+        public void remove() {
+            if (!canRemove) {
+                throw new IllegalStateException("remove() called before next()");
+            }
+            System.arraycopy(table, currentSameHashIndex, table, currentSameHashIndex-1, table.length - currentSameHashIndex);
+            size--;
+            table[table.length -1] = null;
+
+            canRemove = false;
+
+        }
+
+        private void findElement(){
+            if(nextElement!=null){
+                return;
+            }
+            while (currentSameHashIndex < table.length){
+                Object[] sameHash = (Object[])table[currentSameHashIndex];
+                currentElementIndex++;
+                if(sameHash != null && currentElementIndex < sameHash.length && sameHash[currentElementIndex]!=null){
+                    nextElement = (T)sameHash[currentElementIndex];
+                    return;
+                }
+                //если не нашли элемент в массиве, то переходим к следующему
+                currentSameHashIndex++;
+                currentElementIndex=-1;
+            }
+        }
+    }
+
+    @Override
+    public Object[] toArray() { //OK
+        Object[] result = new Object[size];
+        int i = 0;
+        Iterator<T> it = iterator();
+        while (it.hasNext()){
+            result[i++]= it.next();
+        }
+        return result;
+    }
+
+    @Override
+    public <E> E[] toArray(E[] a) {
+        if (a.length < size)
+            a = (E[])java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size);
+        int i = 0;
+        Iterator<T> it = iterator();
+        while (it.hasNext()) {
+            a[i++] = (E) it.next();
+        }
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+    @Override
+    public boolean add(T key) { //OK
+        if (contains(key)){
+            return false;
+        }
+        int index = hash(key);
+        Object[] sameHash = (Object[])table[index];
+
+        if (sameHash == null){
+            table[index] = new Object[]{key};
+        }else{
+            Object[] newSameHash = new Object[sameHash.length+1];
+            System.arraycopy(sameHash,0,newSameHash,0,sameHash.length);
+            newSameHash[newSameHash.length-1] = key;
+            table[index] = newSameHash;
+        }
+        size++;
+        if((float)size/table.length >= LOAD_FACTOR){
+            resize();
+        }
+        return true;
+
+    }
+
+    @Override
+    public boolean remove(Object key) {//OK
+        int index = find(key);
+        if(index == -1){
+            return false;
+        }
+        Object[] sameHash = (Object[])table[index];
+        Object[] newSameHash = new Object[sameHash.length-1];
+        int k = 0;
+        for (int i = 0; i < sameHash.length;i++){
+            if(sameHash[i]!=null && !sameHash[i].equals(key)){
+                newSameHash[k++]=sameHash[i];
+            }
+        }
+        table[index] = (newSameHash.length>0)? newSameHash : null;
+        size--;
+        return true;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for(Object o: c){
+            if(!contains((T)o)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        boolean modified = false;
+        for(T e : c){
+            if(add(e)){
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        Iterator<T> it = iterator();
+        while (it.hasNext()) {
+            T nextElement = it.next();
+            if (!c.contains(nextElement)) {
+                it.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object o : c){
+            if(remove((T)o)){
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public void clear() {
+        table= new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+}
---
+++ src/test/java/ru/spbstu/telematics/java/MyHashSetTest.java
@@ -0,0 +1,144 @@
+package ru.spbstu.telematics.java;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class MyHashSetTest{
+    private MyHashSet<Integer> myHashSet;
+    private HashSet<Integer> regHashSet;
+
+    @Before
+    public void setUp(){
+        myHashSet = new MyHashSet<>();
+        regHashSet = new HashSet<>();
+    }
+
+    @Test
+    public void testSize(){
+        assertEquals(0,myHashSet.size());
+        myHashSet.add(1);
+        myHashSet.add(2);
+        assertEquals(2,myHashSet.size());
+    }
+
+    @Test
+    public void testIsEmpty(){
+        assertEquals(myHashSet.isEmpty(),regHashSet.isEmpty());
+        myHashSet.add(1);
+        myHashSet.add(2);
+        regHashSet.add(1);
+        regHashSet.add(2);
+        assertEquals(myHashSet.isEmpty(),regHashSet.isEmpty());
+    }
+
+    @Test
+    public void testContains() {
+        myHashSet.add(1);
+        myHashSet.add(2);
+        regHashSet.add(1);
+        regHashSet.add(2);
+        assertEquals(myHashSet.contains(1),regHashSet.contains(1));
+        assertEquals(myHashSet.contains(3),regHashSet.contains(3)); //такого объекта нет
+    }
+
+    //ITERATOR CHECK
+
+    @Test
+    public void testToArray() {
+        myHashSet.add(1);
+        myHashSet.add(2);
+        regHashSet.add(1);
+        regHashSet.add(2);
+        Object[] rightArray = {1, 2};
+        assertArrayEquals(rightArray, myHashSet.toArray());
+        assertArrayEquals(regHashSet.toArray(), myHashSet.toArray());
+    }
+
+    @Test
+    public void testToArrayParameterized() {
+        myHashSet.add(1);
+        myHashSet.add(2);
+        myHashSet.add(3);
+        regHashSet.add(1);
+        regHashSet.add(2);
+        regHashSet.add(3);
+        assertArrayEquals(regHashSet.toArray(new Integer[0]),myHashSet.toArray(new Integer[0]));
+        assertArrayEquals(regHashSet.toArray(new Integer[5]),myHashSet.toArray(new Integer[5]));
+    }
+
+    @Test
+    public void testAdd(){
+        assertEquals(myHashSet.add(1),regHashSet.add(1));
+        assertEquals(myHashSet.add(1),regHashSet.add(1)); //проверка, что дубликат не добавится
+        assertEquals(myHashSet.add(2),regHashSet.add(2));
+        assertEquals(myHashSet.size(),regHashSet.size());
+    }
+
+    @Test
+    public void testRemove(){
+        myHashSet.add(1);
+        myHashSet.add(2);
+        regHashSet.add(1);
+        regHashSet.add(2);
+        assertEquals(myHashSet.remove(1),regHashSet.remove(1));
+        assertEquals(myHashSet.remove(3),regHashSet.remove(3)); //такого объекта нет
+        assertEquals(myHashSet.size(),regHashSet.size());
+    }
+
+    @Test
+    public void testContainsAll() {
+        HashSet<Integer> newElements = new HashSet<>(Arrays.asList(4,5,6));
+        myHashSet.addAll(newElements);
+        regHashSet.addAll(newElements);
+        assertEquals(myHashSet.containsAll(newElements),regHashSet.containsAll(newElements));
+    }
+
+    @Test
+    public void testAddAll() {
+        HashSet<Integer> newElements = new HashSet<>(Arrays.asList(4,5,6));
+        myHashSet.addAll(newElements);
+        regHashSet.addAll(newElements);
+        assertEquals(regHashSet.size(),myHashSet.size());
+        assertEquals(myHashSet.containsAll(newElements),regHashSet.containsAll(newElements));
+    }
+
+    @Test
+    public void testRetainAll() {
+        myHashSet.add(1);
+        myHashSet.add(2);
+        myHashSet.add(3);
+        regHashSet.add(1);
+        regHashSet.add(2);
+        regHashSet.add(3);
+        HashSet<Integer> newElements = new HashSet<>(Arrays.asList(1,2,3));
+        assertEquals(myHashSet.retainAll(newElements),regHashSet.retainAll(newElements));
+    }
+
+    @Test
+    public void testRemoveAll() {
+        HashSet<Integer> newElements = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5));
+        HashSet<Integer> removeElements = new HashSet<>(Arrays.asList(1, 3, 5));
+        myHashSet.addAll(newElements);
+        regHashSet.addAll(newElements);
+
+        assertEquals(myHashSet.removeAll(removeElements), regHashSet.removeAll(removeElements));
+        assertEquals(myHashSet.size(), regHashSet.size());
+        assertEquals(myHashSet.containsAll(removeElements), regHashSet.containsAll(removeElements));
+    }
+
+    @Test
+    public void testClear() {
+        HashSet<Integer> newElements = new HashSet<>(Arrays.asList(1, 2, 3));
+        myHashSet.addAll(newElements);
+        regHashSet.addAll(newElements);
+        myHashSet.clear();
+        regHashSet.clear();
+        assertTrue(myHashSet.isEmpty());
+        assertEquals(myHashSet.size(), regHashSet.size());
+    }
+}
